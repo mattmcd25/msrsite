666a745916cf2dafb72699abb768800c0609e9d2
Matt McDonald <mattmcd25@gmail.com>
2018-03-15 15:54:57 +0200

    Deleted Heroku skeleton and started barebones node app

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var http = require('http');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end('Hello world!')
+});
+
+server.listen(8080);
