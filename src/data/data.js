ffcf4a45b698ac21b60538cd8d6afa8b6edcfc6a
Matt McDonald <mattmcd25@gmail.com>
2018-03-19 12:09:37 +0200

    Reorganize files, add MSR icon, and add fake data file for testing before hooking into the database.

---
+++ src/data/data.js
@@ -0,0 +1,36 @@
+const test_data = [
+    {
+        'name': 'Matt',
+        'surname': 'McDonald',
+        'skills': [
+            'Typing',
+            'Programming'
+        ]
+    },
+    {
+        'name': 'Ryan'
+        'surname': 'Wittenberg',
+        'skills': [
+            'Programming',
+            'Hunting'
+        ]
+    },
+    {
+        'name': 'Melissa',
+        'surname': 'Wojnowski',
+        'skills': [
+            'Typing',
+            'Bricklaying'
+        ]
+    },
+    {
+        'name': 'Deanna',
+        'surname': 'Poirier',
+        'skills': [
+            'Typing',
+            'Farming'
+        ]
+    }
+]
+
+export default test_data;
