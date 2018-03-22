8570bc76c0bdde529089886d320bf39208f91c8c
wittenber0 <ryanwwittenberg@gmail.com>
2018-03-22 07:01:23 -0400

    Lift State from MemberTable to IndexPage

--- src/components/MemberTable.js
+++ src/components/MemberTable.js
@@ -1,40 +1,22 @@
 import React from "react";
 import Member from "./Member";
-import { getAllColumns, getAllMembers } from "../data/databaseManager";
 
-export default class MemberTable extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            members: [],
-            headers: [],
-            loaded: false
-        };
-
-        getAllColumns('Member').then(cols => {
-            getAllMembers().then(res => {
-                this.setState({members: res, headers: cols, loaded: true});
-            });
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                {!this.state.loaded ?
-                    <p>Loading database...</p> :
-                    <table>
-                        <thead>
-                        <tr>
-                            {this.state.headers.map(head => <th key={head}>{head}</th>)}
-                        </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.members.map((mem) => <Member data={mem}/>)}
-                        </tbody>
-                    </table>
-                }
-            </div>
-        );
-    }
+export function MemberTable(props) {
+    return (
+        <div>
+            {!props.loaded ?
+                <p>Loading database...</p> :
+                <table>
+                    <thead>
+                    <tr>
+                        {props.headers.map(head => <th key={head}>{head}</th>)}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {props.display.map((mem) => <Member data={mem}/>)}
+                    </tbody>
+                </table>
+            }
+        </div>
+    );
 }
--- src/components/pages/IndexPage.js
+++ src/components/pages/IndexPage.js
@@ -1,10 +1,26 @@
 import React from 'react';
 // import Member from "../Member";
 import { Link } from 'react-router-dom';
-import MemberTable from "../MemberTable";
+import {MemberTable} from "../MemberTable";
+import { getAllColumns, getAllMembers } from "../../data/databaseManager";
 // import DatabaseManager from '../../data/DatabaseManager';
 
 export default class IndexPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            display: [],
+            headers: [],
+            loaded: false
+        };
+
+        getAllColumns('Member').then(cols => {
+            getAllMembers().then(res => {
+                this.setState({display: res, headers: cols, loaded: true});
+            });
+        });
+    }
+
     render() {
         return (
             <div className="home">
@@ -24,7 +40,7 @@ export default class IndexPage extends React.Component {
                     Advanced Search
                 </button>
 
-                <MemberTable/>
+                <MemberTable headers={this.state.headers} display={this.state.display} loaded={this.state.loaded}/>
                 {/*<p>{testDB()}</p>*/}
             </div>
         );
