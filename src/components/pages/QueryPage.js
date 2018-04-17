bbb4fdafc14a4e7d04845745668ae92092a26257
Matt McDonald <mattmcd25@gmail.com>
2018-04-17 14:42:24 +0200

    Refactor searching to reuse code as much as possible. Rewrite readme to no longer include all react information.

--- src/components/pages/QueryPage.js
+++ src/components/pages/QueryPage.js
@@ -72,52 +72,20 @@ export default class QueryPage extends React.Component {
 
     search = async () => {
         this.setState({ mode: 'loading' });
-
         let getID = member => member.ID;
         let promises = [];
 
-        // do general search
-        let genCond = filterObj(this.state.mem);
-        if(Object.keys(genCond).length > 0)
-            promises.push(query('Member', genCond));
+        // searches
+        this.propSearch('mem', 'Member', promises); // general
+        this.state.skills.forEach(NAME => promises.push(query('All_skills', {NAME}))); // skills
+        this.jobSearch('work', 'workSkills', promises); // work
+        this.jobSearch('placement', 'placementSkills', promises); // placements
+        this.jobSearch('training', 'trainingSkills', promises); // training
+        this.propSearch('cert', 'Has_Cert', promises); // certs
+        Object.keys(this.state.langs).forEach( // langs
+            langName => promises.push(query('Know_lang', filterObj(this.state.langs[langName]))));
 
-        // do skills search
-        this.state.skills.forEach(NAME => promises.push(query('All_skills', {NAME})));
-
-        // do work search
-        // this.jobSearch('work', 'workSkills', promises);
-        // this.jobSearch('placement', 'placementSkills', promises);
-        // this.jobSearch('training', 'trainingSkills', promises);
-        let workCond = filterObj(this.state.work);
-        this.state.workSkills.forEach(NAME => promises.push(query('Work_info', {NAME, ...workCond})));
-        if(this.state.workSkills.length === 0 && Object.keys(workCond).length > 0)
-            promises.push(query('Work_info', workCond));
-
-        // do placement search
-        let placeCond = filterObj(this.state.placement);
-        this.state.placementSkills.forEach(NAME => promises.push(query('Placement_info', {NAME, ...placeCond})));
-        if(this.state.placementSkills.length === 0 && Object.keys(placeCond).length > 0)
-            promises.push(query('Placement_info', placeCond));
-
-        // do training search
-        let trainCond = filterObj(this.state.training);
-        this.state.trainingSkills.forEach(NAME => promises.push(query('Training_info', {NAME, ...trainCond})));
-        if(this.state.trainingSkills.length === 0 && Object.keys(trainCond).length > 0)
-            promises.push(query('Training_info', trainCond));
-
-        // do lang search
-        Object.keys(this.state.langs).forEach(langName => {
-            let lang = this.state.langs[langName];
-            let langCond = filterObj(lang);
-            promises.push(query('Know_lang', langCond));
-        });
-
-        // do cert search
-        let certCond = filterObj(this.state.cert);
-        if(Object.keys(certCond).length > 0)
-            promises.push(query('Has_Cert', certCond));
-
-        // send result
+        // compute result
         let allMembers = await getAll('Member');
         let matchingIDs = allMembers.map(getID);
         for(let i in promises) {
@@ -126,6 +94,12 @@ export default class QueryPage extends React.Component {
         }
         let result = allMembers.filter(mem => matchingIDs.includes(mem.ID));
         this.setSearchMode(result);
+    };
+
+    propSearch = (set, table, promises) => {
+        let cond = filterObj(this.state[set]);
+        if(Object.keys(cond).length > 0)
+            promises.push(query(table, cond));
     };
 
     jobSearch = (set, skillSet, promises) => {
